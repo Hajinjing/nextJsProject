7a5d64dc94dc9bc65cd96bef09fcfe5c5584b70f
MDP0038-at-780618930571 <hazzing@mz.co.kr>
2023-11-22 17:48:09 +0900

    component/props/useState

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,7 +1,10 @@
+'use client'
 import Image from 'next/image'
 import 작명 from '/public/food0.png'
+import { useState } from "react"
 export default function List() {
-  let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+    let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+    let [수량, 수량변경] = useState(0)
   return (
       <div>
           <h4 className="title">상품목록</h4>
@@ -11,6 +14,9 @@ export default function List() {
                       <div className="food" key={i}>
                           <img src={`/food${i}.png`} className="food-img"/>
                           <h4>{ el } $40</h4>
+                          <span> {수량} </span>
+                          <button onClick={()=>{수량변경(수량+1)}}>+</button>
+                          <button onClick={()=>{수량변경(수량-1)}}>-</button>
                       </div>
                   )
               })
