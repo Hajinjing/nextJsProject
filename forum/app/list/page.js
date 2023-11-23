bd3a9418dc3abef9ec5c827fb6fde8d761e45027
MDP0038-at-780618930571 <hazzing@mz.co.kr>
2023-11-23 17:01:55 +0900

    Dynamic route/useRouter

--- forum/app/list/page.js
+++ forum/app/list/page.js
@@ -1,24 +1,28 @@
+// 'use client'
 import {connectDB} from "@/util/database";
-
+import Link from "next/link"
+import DetailLink from "@/app/list/DetailLink";
 export default async function List() {
 
     const client = await connectDB;
     const db = client.db("forum")
 
     let result = await db.collection('post').find().toArray()
+    console.log(result[0]._id)
 
     return (
         <div className="list-bg">
             {
-                result.map(r=>{
-                    return (
-                        <div className="list-item">
-                            <h4>{r.title}</h4>
-                            <p>{r.content}</p>
-                        </div>
+                result.map((r,i)=>
 
-                    )
-                })
+                        <div className="list-item" key={i}>
+                            <Link href={`detail/${r._id}`}>
+                                <h4>{r.title}</h4>
+                            </Link>
+                            <DetailLink/>
+                            <p>1월 1일</p>
+                        </div>
+                )
             }
         </div>
     )
