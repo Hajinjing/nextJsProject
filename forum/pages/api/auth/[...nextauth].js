69664ad4ce4d95da6b5bdc847a0331fd1f9acf62
MDP0038-at-780618930571 <hazzing@mz.co.kr>
2023-11-27 15:59:59 +0900

    회원가입 2(OAuth+session)

--- forum/pages/api/auth/[...nextauth].js
+++ forum/pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import {connectDB} from "@/util/database";
 
 export const authOptions = {
     providers: [
@@ -8,7 +10,8 @@ export const authOptions = {
             clientSecret: '318d62ff75d5871c04a05aa0f2d851d5c9d6e75f',
         }),
     ],
-    secret : 'gkwlswkd!159'
+    secret : 'gkwlswkd!159',
+    adapter : MongoDBAdapter(connectDB)
 
 };
 export default NextAuth(authOptions);
--- forum/pages/api/post/new.js
+++ forum/pages/api/post/new.js
@@ -1,6 +1,14 @@
 import {connectDB} from "@/util/database";
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/pages/api/auth/[...nextauth]";
+
 
 export default async function handler(요청, 응답) {
+    let session = await getServerSession(요청, 응답, authOptions)
+    if (session) {
+        요청.body.author = session.user.email
+    }
+    // console.log(요청.body)
     if (요청.method == 'POST') {
         console.log(요청.body)
         if (요청.body.title == '') {
